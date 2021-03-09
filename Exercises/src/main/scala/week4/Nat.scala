39e3b15807d2075fc8986ca954431d808964258e
Maxwell Daum <mdaum@trueaccord.com>
2021-03-09 12:38:09 -0500

    4.1 exercises

---
+++ Exercises/src/main/scala/week4/Nat.scala
@@ -0,0 +1,23 @@
+package week4
+
+abstract class Nat {
+  def isZero: Boolean
+  def predecessor: Nat
+  def successor: Nat = new Succ(this)
+  def +(that: Nat): Nat
+  def -(that: Nat): Nat
+}
+
+class Succ(n: Nat) extends Nat {
+  def isZero = false
+  def predecessor = n
+  def +(that: Nat): Nat = new Succ(n + that)
+  def -(that: Nat): Nat = if (that.isZero) this else n - that.predecessor
+}
+
+object Zero extends Nat {
+  def isZero = true
+  def predecessor = throw new Error("0.predecessor")
+  def +(that: Nat) = that
+  def -(that: Nat) = if (that.isZero) this else throw new Error("negative number")
+}
